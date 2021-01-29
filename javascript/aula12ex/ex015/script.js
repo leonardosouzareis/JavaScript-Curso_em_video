1b5652f73894286b431270d0243b780bc4182aa7
Leonardo S Reis <leonardokings@hotmail.com>
2021-01-29 01:23:45 -0300

    Update

--- javascript/aula12ex/ex015/script.js
+++ javascript/aula12ex/ex015/script.js
@@ -10,30 +10,41 @@ function verificar() {
         var fSex = document.getElementsByName('radsex') // f == Formulário
         var idade = ano - Number(fAno.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
 
         if (fSex[0].checked) {
             genero = 'Homem'
-            if (idade>0 && idade<11) {
+            if (idade>0 && idade<16) {
                 // Criança
-            } else if (idade <25) {
+                img.setAttribute('src', 'fotos/bebe-o.png')
+            } else if (idade <23) {
                 // Jovem
+                img.setAttribute('src', 'fotos/jovem-o.png')
             } else if (idade<51) {
                 // Adulto
+                img.setAttribute('src', 'fotos/adulto.png')
             } else {
                 // Idoso
+                img.setAttribute('src', 'fotos/idoso.png')
             }
         } else {
             genero = 'Mulher'
-            if (idade>0 && idade<11) {
+            if (idade>0 && idade<16) {
                 // Criança
-            } else if (idade <25) {
+                img.setAttribute('src', 'fotos/bebe-a.png')
+            } else if (idade <23) {
                 // Jovem
+                img.setAttribute('src', 'fotos/jovem-a.png')
             } else if (idade<51) {
                 // Adulto
+                img.setAttribute('src', 'fotos/adulta.png')
             } else {
                 // Idoso
+                img.setAttribute('src', 'fotos/idosa.png')
             }
         }
         res.innerHTML = `${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
