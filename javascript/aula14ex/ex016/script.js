6d91f6c76f4f8e1d1d0d86a83579ed16171bd32a
Leonardo S Reis <leonardokings@hotmail.com>
2021-02-03 22:41:50 -0300

    Update

--- javascript/aula14ex/ex016/script.js
+++ javascript/aula14ex/ex016/script.js
@@ -1,1 +1,32 @@
-alert("Olá, seja bem vindo!")
+function contar() {
+    let inicio = document.getElementById('numI')
+    let fim = document.getElementById('numF')
+    let passo = document.getElementById('passo')
+    let res = document.getElementById('contagem')
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Verifique se preencheu todos os campos!')
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if (p <= 0) {
+            window.alert('[ERRO] O passo não pode ser menor ou igual a 0 (zero). Iremos considerar passo igual a 1 (um).')
+            p = 1
+        }
+        // Contagem crescente
+        if (i < f) {
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        // Contagem decrescente 
+        } else {
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            } 
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
