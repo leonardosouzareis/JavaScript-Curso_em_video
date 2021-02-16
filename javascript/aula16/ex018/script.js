afb277d86c2b8f6849ec26c01fced8891fcdb677
Leonardo S Reis <leonardokings@hotmail.com>
2021-02-16 00:36:34 -0300

    Update

--- javascript/aula16/ex018/script.js
+++ javascript/aula16/ex018/script.js
@@ -2,3 +2,27 @@ var n = document.getElementById('fnum')
 var lista = document.querySelector('select#flista')
 var res = document.querySelector('div#res')
 var valores = []
+
+funcition isNum(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inList(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true 
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if(isNum(num.value) && !inList(num.value, valores)) {
+
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
+}
