de50b62fb18f6b01cd6ee9d852784c137ad3d0b4
Leonardo S Reis <leonardokings@hotmail.com>
2021-02-16 10:25:53 -0300

    Update

--- javascript/aula16/ex018/script.js
+++ javascript/aula16/ex018/script.js
@@ -25,6 +25,7 @@ function adicionar() {
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado.`
         lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         window.alert('Valor inválido ou já encontrado na lista.')
     }
@@ -33,5 +34,31 @@ function adicionar() {
 }
 
 function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores.')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
 
+        for (let pos in valores) {
+            soma += valores[pos]
+
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+
+        media = soma / valores.length
+        
+        res.innerHTML = ''
+        res.innerHTML += `<p>Você adicionou ${tot} valores.</p>`
+        res.innerHTML += `<p>O MAIOR valor é ${maior}.</p>`
+        res.innerHTML += `<p>O MENOR valor é ${menor}.</p>`
+        res.innerHTML += `<p>A soma dos valores é ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores é ${media}.</p>`
+    }
 }
